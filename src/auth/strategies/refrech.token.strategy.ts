b9ff3b9bc8f07e7e8284e72247c84fe9f14934b5
Sidali Assoul <53428196+stormsidali2001@users.noreply.github.com>
2022-07-22 22:25:15 +0200

    Update refrech.token.strategy.ts

--- src/auth/strategies/refrech.token.strategy.ts
+++ src/auth/strategies/refrech.token.strategy.ts
@@ -10,7 +10,7 @@ export class RefrechTokenStrategy extends PassportStrategy(Strategy,'jwt-refrech
     constructor(config:ConfigService){
         super({
             jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey:config.get<string>('ACCESS_TOKEN_SECRET'),
+            secretOrKey:config.get<string>('REFRECH_TOKEN_SECRET'),
             passReqToCallback:true
         })
     }
